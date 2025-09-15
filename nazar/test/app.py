600a6089cfb221f84047b298b05229b332ba744f
rr0x9 <220593292+rr0x9@users.noreply.github.com>
2025-09-15 20:49:41 +0100

    playing with lab

--- nazar/test/app.py
+++ nazar/test/app.py
@@ -3,7 +3,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return """
+    <h1>Hello, World!!</h1>
+    <i>italic stuff</i>
+    <button id="btn">Click</button>
+    <script>
+        document.getElementById('btn').addEventListener('click', function() {
+            alert('clicked');
+        })
+    </script>
+    """\
+    
+@app.route("/<name>")
+def helloName(name):
+    return f"Hello, {name}!!"
 
 if __name__ == "__main__":
     app.run(port=8000, debug=True)
